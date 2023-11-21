73672e28530e9d50997b3f13b5f05343d860b935
Fatih Kurt <fthbtlkrt@gmail.com>
2023-11-21 02:55:52 +0100

    production

--- sanity.cli.js
+++ sanity.cli.js
@@ -5,6 +5,6 @@
 import { defineCliConfig } from 'sanity/cli';
 
 const projectId = 'veclq2f6'; //process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const dataset = 'production'; //process.env.NEXT_PUBLIC_SANITY_DATASET;
 
 export default defineCliConfig({ api: { projectId, dataset } });
