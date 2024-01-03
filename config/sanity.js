fb38c8019347b7d16eae88fccbe3c7336811b421
Fatih Kurt <fthbtlkrt@gmail.com>
2024-01-03 02:40:19 +0100

    change credentials for connection with sanit client

--- config/sanity.js
+++ config/sanity.js
@@ -4,16 +4,15 @@ import schemas from '../sanity/schemas';
 import { visionTool } from '@sanity/vision';
 
 const config = defineConfig({
-  projectId: 'veclq2f6', //process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: 'production',
   title: 'SalesTeam API',
   apiVersion: '2023-06-26',
-  token:
-    'skNsWWZEMhtwAtiVBrLXHonDESLvBbj1IUEaovZfsE35sG2flK6pw9mEtqFoqJ2hyAdywVNCP0xz4bqhlCt7mb4moplZ7wl5zNOFg6KAvz3S9WVWKabsQZjgVByoZny3BG5dBm6cPMxkhpiXBJ6N9RIi9w5Mp2qLj89z6lPmC8Puie1yVAm7', //process.env.NEXT_PUBLIC_SANITY_TOKEN,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
   basePath: '/studio',
   plugins: [deskTool(), visionTool()],
   schema: { types: schemas },
-  useCdn: false, //true, //process.env.NODE_ENV === 'production',
+  useCdn: false, //process.env.NODE_ENV === 'production',
 });
 
 export default config;
