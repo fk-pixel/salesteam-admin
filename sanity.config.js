2b7702190cf66a78a6fbce5baac0aad21f75c3cb
Fatih Kurt <fthbtlkrt@gmail.com>
2024-01-03 02:47:18 +0100

    fix cdn settings from client js file

--- sanity.config.js
+++ sanity.config.js
@@ -11,12 +11,11 @@ import schemas from './sanity/schemas';
 
 export default defineConfig({
   basePath: '/studio',
-  projectId: 'veclq2f6', //process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: 'production', //process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: 'production',
   title: 'SalesTeam API',
-  token:
-    'skNsWWZEMhtwAtiVBrLXHonDESLvBbj1IUEaovZfsE35sG2flK6pw9mEtqFoqJ2hyAdywVNCP0xz4bqhlCt7mb4moplZ7wl5zNOFg6KAvz3S9WVWKabsQZjgVByoZny3BG5dBm6cPMxkhpiXBJ6N9RIi9w5Mp2qLj89z6lPmC8Puie1yVAm7', //process.env.NEXT_PUBLIC_SANITY_TOKEN,
-  useCdn: false, //true, //process.env.NODE_ENV === 'production',
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+  useCdn: false, //process.env.NODE_ENV === 'production',
   schema: { types: schemas },
   plugins: [
     deskTool(),
