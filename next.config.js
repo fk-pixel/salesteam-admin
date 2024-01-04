d2c02578440d3ecd08e24588a9da75c249fb74d9
Fatih Kurt <fthbtlkrt@gmail.com>
2024-01-04 03:41:55 +0100

    add webpack config for xlsx lib

--- next.config.js
+++ next.config.js
@@ -31,6 +31,16 @@ module.exports = {
   //   const sanityRedirects = await fetchSanityRedirects();
   //   return sanityRedirects;
   // },
+  webpack: (config) => {
+    /* add to the webpack config module.rules array */
+    config.module.rules.push({
+      /* `test` matches file extensions */
+      test: /\.(numbers|xls|xlsx|xlsb)/,
+      /* use the loader script */
+      use: [{ loader: './base64-loader' }],
+    });
+    return config;
+  },
   reactStrictMode: true,
   swcMinify: false,
   compiler: {
